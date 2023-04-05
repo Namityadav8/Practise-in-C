b13148d4a18de474caa3f2892a56fcdf9c10d9b3
Namit Yadav <114800158+Namityadav8@users.noreply.github.com>
2023-04-05 22:24:11 +0530

    some of the questions of c

---
+++ ques1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Name: Namit Yadav\n");
+    printf("Registration number: 12217663\n");
+    printf("Enter Your CGPA:5\n");
+    return 0;
+}
---
+++ ques2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// #include<conin.h>
+
+int main(){
+    char name[20]="Namit";
+    int roll_no = 13;
+    float cgpa = 9.2;
+    printf("name = %s\n");
+    printf("roll no.  = %d\n", roll_no);
+    printf("your cgpa = %f\n ");
+    return 0;
+}
